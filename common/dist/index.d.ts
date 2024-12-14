77535f7012c1e58ed9c5d4b4e5290fa9f707b906
Manjinder <msinghw3@gmail.com>
2024-12-13 21:37:19 -0600

    Added Zod validations in Common module

---
+++ common/dist/index.d.ts
@@ -0,0 +1,4 @@
+import { signupInput, SignupInput, signinInput, SigninInput } from "./user";
+import { createBlogInput, CreateBlogInput, updateBlogInput, UpdateBlogInput } from "./blog";
+export { signupInput, SignupInput, signinInput, SigninInput };
+export { createBlogInput, CreateBlogInput, updateBlogInput, UpdateBlogInput };
---
+++ common/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateBlogInput = exports.createBlogInput = exports.signinInput = exports.signupInput = void 0;
+const user_1 = require("./user");
+Object.defineProperty(exports, "signupInput", { enumerable: true, get: function () { return user_1.signupInput; } });
+Object.defineProperty(exports, "signinInput", { enumerable: true, get: function () { return user_1.signinInput; } });
+const blog_1 = require("./blog");
+Object.defineProperty(exports, "createBlogInput", { enumerable: true, get: function () { return blog_1.createBlogInput; } });
+Object.defineProperty(exports, "updateBlogInput", { enumerable: true, get: function () { return blog_1.updateBlogInput; } });
